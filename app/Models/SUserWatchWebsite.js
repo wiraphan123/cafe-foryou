46abb7ad2518c6d4582c6fa50fba34cb6ee0c3af
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-09-12 17:04:58 +0700

    add/ ModelsUser_watch_website

---
+++ app/Models/SUserWatchWebsite.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class SUserWatchWebsite extends Model {
+}
+
+module.exports = SUserWatchWebsite
