2895ebd60ea3d0c2a4fcf9d6710e92b7045fddb4
pondatikan001 <pondatikan01@gmail.com>
2020-09-12 23:47:58 +0700

    edit model/adminupdatewebsite

--- app/Models/SAdminUpdateWebsite.js
+++ app/Models/SAdminUpdateWebsite.js
@@ -4,6 +4,18 @@
 const Model = use('Model')
 
 class SAdminUpdateWebsite extends Model {
+    static get primaryKey() {
+        return 'admin_update_website_id'
+    }
+    static get createdAtColumn() {
+        return null;
+    }
+    static get updatedAtColumn() {
+        return null;
+    }
+    admin() {
+        return this.belongsTo('App/Models/Admin')
+    }
 }
 
 module.exports = SAdminUpdateWebsite
