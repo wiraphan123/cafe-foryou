1e44576c193615c560df10e37f690cb8e8b1c48b
pondatikan001 <pondatikan01@gmail.com>
2020-09-12 23:23:17 +0700

    add util/admin

---
+++ database/util/adminUtil.js
@@ -0,0 +1,43 @@
+class adminUtil {
+
+    constructor(AdminModel) {
+        this._Admin = AdminModel
+    }
+
+    getAll(references) {
+        const admins = this._Admin.query()
+
+        return this._withReference(admins, references)
+            .fetch()
+    }
+
+    getById(adminId, references) {
+        const admin = this._Admin
+            .query()
+            .where('admin_id', adminId)
+
+        return this._withReference(admin, references)
+            .fetch()
+            .then(response => response.first())
+    }
+
+    async create(adminInstance, references) {
+        const { admin_id } = await this._Admin.create(adminInstance)
+        const admin = this._Admin
+            .query()
+            .where('admin_id', admin_id)
+
+        return this._withReference(admin, references)
+            .fetch()
+            .then(response => response.first())
+    }
+
+    _withReference(instance, references) {
+        if (references) {
+            const extractedReferences = references.split(",")
+            instance.with(extractedReferences)
+        }
+
+        return instance
+    }
+}
