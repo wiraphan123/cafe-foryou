a4d34d12740b22ddf0f7948bb0ddb7b7917f35b8
pondatikan001 <pondatikan01@gmail.com>
2020-09-12 21:28:52 +0700

    edit/model community

--- app/Models/SCommunity.js
+++ app/Models/SCommunity.js
@@ -4,6 +4,16 @@
 const Model = use('Model')
 
 class SCommunity extends Model {
+    static get primaryKey() {
+        return 'community_id'
+    }
+    static get createdAtColumn() {
+        return null;
+    }
+    static get updatedAtColumn() {
+        return null;
+    }
 }
+
 
 module.exports = SCommunity
