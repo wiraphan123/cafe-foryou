23552bc13ba284004ac7fc4398500fb44d116fb7
pondatikan001 <pondatikan01@gmail.com>
2020-09-12 23:38:28 +0700

    add util/cafe

---
+++ database/util/CafeUtil.js
@@ -0,0 +1,43 @@
+class CafeUtil {
+
+    constructor(CafeModel) {
+        this._Cafe = CafeModel
+    }
+
+    getAll(references) {
+        const cafe = this._Cafe.query()
+
+        return this._withReference(cafe, references)
+            .fetch()
+    }
+
+    getById(cafeId, references) {
+        const cafe = this._Cafe
+            .query()
+            .where('cafe_id', cafeId)
+
+        return this._withReference(cafe, references)
+            .fetch()
+            .then(response => response.first())
+    }
+
+    async create(cafeInstance, references) {
+        const { cafe_id } = await this._Cafe.create(cafeInstance)
+        const cafe = this._Cafe
+            .query()
+            .where('cafe_id', cafe_id)
+
+        return this._withReference(cafe, references)
+            .fetch()
+            .then(response => response.first())
+    }
+
+    _withReference(instance, references) {
+        if (references) {
+            const extractedReferences = references.split(",")
+            instance.with(extractedReferences)
+        }
+
+        return instance
+    }
+}
