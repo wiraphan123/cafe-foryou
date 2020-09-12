f391d96852f667cf4654b80132b14910f6768a8c
pondatikan001 <pondatikan01@gmail.com>
2020-09-12 13:56:55 +0700

    add migration cafe

---
+++ database/migrations/1599893686175_create_cafe_schema.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class CreateCafeSchema extends Schema {
+  up () {
+    this.create('create_cafes', (table) => {
+      table.increments('cafe_id', 5)
+      table.string('cafe_name', 150).notNullable()
+      table.string('detail', 150).notNullable()
+      table.string('comment_review', 100).notNullable()
+      table.integer('user_id').notNullable().unsigned()
+      table.integer('admin_id').notNullable().unsigned()
+      table.foreign('user_id')
+          .references('users.user_id')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE')
+      table.foreign('admin_id')
+          .references('admins.admin_id')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE')
+      table.timestamps()
+  })
+}
+
+  down () {
+    this.drop('create_cafes')
+  }
+}
+
+module.exports = CreateCafeSchema
