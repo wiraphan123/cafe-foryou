709169770c5e0f54614f4fa7863379bc2d47cdac
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-09-12 16:55:18 +0700

    add/seed

---
+++ database/seeds/DatabasisSeeder.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| DatabasisSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+class DatabasisSeeder {
+  async run () {
+  }
+}
+
+module.exports = DatabasisSeeder
