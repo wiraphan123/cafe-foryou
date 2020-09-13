016e116fcc52866574f89ff1888fb25fb279ff5e
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-09-13 22:45:04 +0700

    edit/Models/ClientsWatchCafe

--- app/Models/SClientsWatchCafe.js
+++ app/Models/SClientsWatchCafe.js
@@ -6,8 +6,10 @@ const Model = use('Model')
 class SClientsWatchCafe extends Model {
     static get primaryKey() {
         return 'Clients_Watch_Cafe_id'
-    client()
+    }
+    clientWatchCafe(){
         return this.belongsTo('App/Models/Client_Watch_Cafe')
+    }
     }
 }
 module.exports = SClientsWatchCafe
