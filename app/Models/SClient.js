903d06b3ea6b4cad44d69cc94ab7352f02bbb61b
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-09-13 12:58:00 +0700

    edit/Models-Client

--- app/Models/SClient.js
+++ app/Models/SClient.js
@@ -4,6 +4,14 @@
 const Model = use('Model')
 
 class SClient extends Model {
+    static get primaryKey() {
+        return 'Usename_id'
 }
-
+static get primaryKey() {
+    return 'password'
+}
+admin() {
+    return this.belongsTo('App/Models/Admin')
+}
+}
 module.exports = SClient
