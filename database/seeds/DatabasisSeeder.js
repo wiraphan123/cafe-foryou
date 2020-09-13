d1bc81b643efe0369268835fd1087855276f0e74
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-09-13 19:38:21 +0700

    edit/DatabasisSeeder

--- database/seeds/DatabasisSeeder.js
+++ database/seeds/DatabasisSeeder.js
@@ -15,9 +15,33 @@ const Factory = use('Factory')
 
 class DatabasisSeeder {
   async run () {
-    const admin= await Factory
+    const admin = await Factory
     .model('App/Models/Admin')
     .createMany(10)
+    
+    const admin_update_website = await Factory
+    .model('App/Models/admin_Updata_Website')
+    .createMany(10)
+    
+    const client = await Factory
+    .model('App/Models/Client')
+    .createMany(10)
+
+    const client_watch_cafe = await Factory
+    .model('App/Models/Client_Watch_Cafe')
+    .createMany(10)
+
+    const posts = await Factory
+    .modle('App/Modls/Posts')
+    .createMany(10)
+
+    const community = await Factory
+    .model('App/Models/Community')
+    .createMany(10)
+
+    const post_community = await Factory
+    .model('App/Model/Post_Community')
+    .createMany(10)s
   }
 }
 
