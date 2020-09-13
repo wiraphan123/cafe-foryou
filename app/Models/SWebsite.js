f249df26fc2f097407ea1eb1f1514bd92a5d4c13
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-09-13 13:05:49 +0700

    edit/Models-website

--- app/Models/SWebsite.js
+++ app/Models/SWebsite.js
@@ -4,6 +4,15 @@
 const Model = use('Model')
 
 class SWebsite extends Model {
+    static get primaryKey() {
+        return 'admin_id'
+}
+static get primaryKey() {
+    return 'usename_id'
+}
+static get primaryKey() {
+    return 'community_id'
+}
 }
 
 module.exports = SWebsite
