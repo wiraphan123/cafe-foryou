b43ed9d9be4f9b1f7f54063b22a9edf1ec7892bf
pondatikan001 <pondatikan01@gmail.com>
2020-09-15 02:54:34 +0700

    edit controller/community

--- app/Controllers/Http/CommunityController.js
+++ app/Controllers/Http/CommunityController.js
@@ -5,11 +5,6 @@ const Community =use("App/Models/Community")
 const CommunityValidator = require("../../../service/CommunityValidator")
 const CommunityUtill = require("../../../util/Community")
 
-function numberTypeParamValidator(number) {
-    if(Number.isNaN(parseInt(number))) 
-        return { error:  `param: ${number} is not support, Pleasr use number type param instead. ` }
-    return {}
-}
 
 class CommunityController {
         async index(){
@@ -22,37 +17,46 @@ class CommunityController {
         async show({request}){
             const { id } = request.params
             const { references } = request.qs
-            const validatedValue = numberTypeParamValidator(id)
-            if (validatedValue.error)
-            return { status: 500, error: validatedValue.error, data: undefined }
-
+            CommunityValidator(references)
+            
             const communityUtill = new CommunityUtill(Community)
-            const community = communityUtill.getById(id, references)
+            const community = await communityUtill.getById(id, references)
             return{ status: 200, error : undefined, data : community ||{} }
 
         }
         async store({ request }) {
             const { post,comment_post } = request.body
             const { references } = request.qs
+            const validation = await CommunityValidator(request.body)
+                if (validation.error){
+                    return { status: 422, error: validated.error, data: undefined }
+                }
             const communityUtill = new CommunityUtill(Community)
             const community = await communityUtill.create({ post,comment_post }, references)
     
             return { status: 200, error: undefined, data: community }
         }
         async update({request}){
-            const {body,params}=request
-            const {id}=params
-            const communityId = await Database
-            .table('communitys')
-            .where({community_id:id})
+            const {references = undefined} =request.qs
+            const validation = await communityValidator(request.body)
+            if (validation.error){
+                return { status: 422, error: validated.error, data: undefined }
+            }
+            const communityUtil = new CommunityUtil(Community)
+            const comunity = await communityUtil
+                .updateById(request,references)
     
-            const community = await Database 
-            .table('communitys')
-            .where({community_id:studentId})
-            .first()
-            return{status : 200,error : undefined , data : { community } }
-        }
+                return { status: 200, error: undefined, data: community }
+      }
+      async destroy({ request }) {
+        const {references = undefined} =request.qs
+        const communityUtil = new CommunityUtil(Community)
+        const community = await communityUtil.deletById(request,references)
+
+        return { status: 200, error: undefined, data: { maessage: 'success' } }
+    }
 }
+
 
 
 module.exports = CommunityController
