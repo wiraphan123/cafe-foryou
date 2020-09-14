220df5a5d2c548ed63ba41263a752a40e3067f24
pondatikan001 <pondatikan01@gmail.com>
2020-09-15 03:20:01 +0700

    edit controller/admin

--- app/Controllers/Http/AdminController.js
+++ app/Controllers/Http/AdminController.js
@@ -6,12 +6,6 @@ const Admin = use('App/Models/Admin')
 const AdminUtil = require("../../../util/adminUtil")
 const Hash = use('Hash')
 
-function numberTypeParamValidator(number) {
-    if (Number.isNaN(parseInt(number)))
-        return { error: `param: ${number} is not supported, please use number type param instead.` }
-    return {}
-}
-
 class AdminController {
     async index({ request }) {
         const { references } = request.qs
@@ -22,56 +16,46 @@ class AdminController {
     async show({ request }) {
         const { id } = request.params
         const { references } = request.qs
-        const validatedValue = numberTypeParamValidator(id)
-        if (validatedValue.error)
-            return { status: 500, error: validatedValue.error, data: undefined }
-
-        const adminUtil = new AdminUtil(Admin)
-        const admins = adminUtil.getById(id, references)
-        return { status: 200, error: undefined, data: admins || {} }
-    }
+        AdminValidator(id)
+        const adminUtil = new AdminUtil(Category)
+        const admin = await adminUtil.getById(id,references)
+        
+        return { status: 200, error: undefined, data: admin || {} }
+        }
+        
+    
         async store({ request }) {
-            const { first_name, last_name, age, gender, admin_name, password,  } = request.body
             const { references } = request.qs
             const validatedData = await AdminValidator(request.body)
-            if (validatedData.error)
+            if (validatedData.error){
                 return { status: 422, error: validatedData.error, data: undefined }
-                const adminUtil = new AdminUtil(Admin)
-        const admins = await adminUtil.create({ first_name, last_name, age, admin_name, password, status }, references)
+        }
+            const adminUtil = new AdminUtil(Admin)
+            const admins = await adminUtil.create({ first_name, last_name, age, admin_name, password, status }, references)
 
         return { status: 200, error: undefined, data: admins }
-
+    
     }
     async update({ request }) {
+        const {references = undefined} =request.qs
+        const validatedData = await AdminValidator(request.body)
+        if (validatedData.error){
+            return { status: 422, error: validatedData.error, data: undefined }
+        }
+        const adminUtil = new AdminUtil(admin)
+        const admin = await adminUtil
+        .updateById(request,references)
 
-        const { body, params } = request
-        const { id } = params
-        const { first_name, last_name, age, gender, admin_name, password,} = body
-
-        const hashedPassword = await Hash.make(password)
-
-        const adminId = await Database
-            .table('admins')
-            .where({ admin_id: id })
-            .update({ first_name, last_name, age, gender, admin_name, password: hashedPassword,  })
-
-        const admins = await Database
-            .table('admins')
-            .where({ admin_id: adminId })
-            .first()
-
-        return { status: 200, error: undefined, data: admins }
+        return { status: 200, error: undefined, data: admin }
     }
     async destroy({ request }) {
-        const { id } = request.params
-
-        await Database
-            .table('admins')
-            .where({ admin_id: id })
-            .delete()
+        const {references = undefined} =request.qs
+        const adminUtil = new AdminUtil(Admin)
+        const admin= await adminUtil.deletById(request,references)
 
         return { status: 200, error: undefined, data: { maessage: 'success' } }
     }
 }
 
+       
 module.exports = AdminController
