ed6d69e30591bc85d2228d7116b61abf38d515f2
pondatikan001 <pondatikan01@gmail.com>
2020-09-15 01:09:00 +0700

    edit util/admin

--- database/util/adminUtil.js
+++ database/util/adminUtil.js
@@ -1,3 +1,5 @@
+const CommunityUtil = require("./CommunityUtil")
+
 class AdminUtil {
 
     constructor(AdminModel) {
@@ -30,6 +32,35 @@ class AdminUtil {
         return this._withReference(admin, references)
             .fetch()
             .then(response => response.first())
+    }
+
+    async deletById(adminInstance){
+        const { id } = adminInstance.params
+        const admin = await this._Admin.find(id)
+
+        if(!admin){
+            return {status : 500 ,error : `Not Found ${id}` , data : undefined};
+        }
+        admin.delete()
+        await admin.save();
+
+        return {status : 200 ,error : undefined , data : 'complete'};
+    }
+
+    async updateById(adminInstance,references){
+        const { id } = adminInstance.params
+        let admin = await this._Admin.find(id)
+
+        if(!admin){
+            return {status : 500 ,error : `Not Found ${id}` , data : undefined};
+        }
+
+        admin.merge(adminInstance.body)
+        await admin.save();
+    
+        admin = this._Admin.query().where({admin_id : id})
+        
+        return this._withReferences(community,references).fetch().then(response => response.first())
     }
 
     _withReference(instance, references) {
