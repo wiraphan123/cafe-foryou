3d7dae0fe518e16c2a6b559320082551bf4a7f7b
wiraphan123 <wiraphan_blas_123@hotmail.com>
2020-09-14 20:14:58 +0700

    delete/AdminController

--- app/Controllers/Http/AdminController.js
+++ app/Controllers/Http/AdminController.js
@@ -4,18 +4,6 @@ class AdminController {
     async store({ request }) {
         const { news, detail } = request.body
 
-        const rules = {
-            news: 'required',
-            datail: 'required',
-        }
-        const validation = await Validator.validateAll(request.body, rules)
-
-        if (validation.fails())
-            return { status: 422, error: validation.messages(), data: undefined }
-
-        const subject = new Subject();
-        subject.admin_id = admin_id;
-        await subject.save()
 
         return { status: 200, error: undefined, data: subject }
 
