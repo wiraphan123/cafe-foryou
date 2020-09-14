fc04ceb5861da2dcb0c5d4dc295098f58d306992
pondatikan001 <pondatikan01@gmail.com>
2020-09-15 01:33:31 +0700

    edit controller/user

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,19 +1,29 @@
 'use strict'
 
+const UserValidator = require("../../../service/UserValidator")
 const Database = use('Database')
 const User = use('App/Models/User')
 const UserUtil = require('../../../util/UserUtil')
+
+function numberTypeParamValidator(number) {
+    if(Number.isNaN(parseInt(number))) 
+        return { error:  `param: ${number} is not support, Pleasr use number type param instead. ` }
+    return {}
+}
 
 class UserController {
     async index(){
         const { references } = request.qs
         const userUtill = new UserUtill(User)
         const user = await userUtill.getAll(references)
-        return { status : 200 , error : undefined, data : group}
+        return { status : 200 , error : undefined, data : user }
     }
     async show({request}){
         const { id } = request.params
         const { references } = request.qs
+        const validatedValue = numberTypeParamValidator(id)
+        if (validatedValue.error)
+            return { status: 500, error: validatedValue.error, data: undefined }
         constuserUtill = new UserUtill(User)
         const user = userUtill.getById(id, references)
         return{ status: 200, error : undefined, data : user ||{} }
@@ -21,6 +31,9 @@ class UserController {
     async store ({request}){
         const {first_name,last_name,age,gender,user_name,password,} = request.body
         const { references } = request.qs
+        const validatedData = await UserValidator(request.body)
+        if (validatedData.error)
+            return { status: 422, error: validatedData.error, data: undefined }
         const userUtill = new UserUtill(User)
         const user = await userUtill.create({first_name,last_name,age,gender,user_name,password,}, references)
         return { status: 200, error: undefined, data: user }
@@ -28,12 +41,14 @@ class UserController {
     async update({ request }) {
         const { body, params } = request
         const { id } = params
-        const { first_name, last_name, age, gender, user_name, password, status } = body
+        const { first_name, last_name, age, gender, user_name, password, } = body
+        
         const userId = await Database
             .table('users')
             .where({ user_id: id })
-            .update({ first_name, last_name, age, gender, user_name, password, status })
-        const user = await Database
+            .update({ first_name, last_name, age, gender, user_name, password, })
+        
+            const user = await Database
             .table('users')
             .where({ user_id:userId })
             .first()
