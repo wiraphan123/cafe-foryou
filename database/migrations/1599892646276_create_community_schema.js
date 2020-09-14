ce6365890de92ea469984445f81a7000cc910da4
pondatikan001 <pondatikan01@gmail.com>
2020-09-15 05:45:10 +0700

    edit migration/community

--- database/migrations/1599892646276_create_community_schema.js
+++ database/migrations/1599892646276_create_community_schema.js
@@ -3,36 +3,23 @@
 /** @type {import('@adonisjs/lucid/src/Schema')} */
 const Schema = use('Schema')
 
-class CreateUserpostcommunitySchema extends Schema {
+class CreateCommunitySchema extends Schema {
   up () {
-    this.create('create_userpostcommunities', (table) => {
-      table.increments()
+    this.create('communities', (table) => {
+      table.increments('community_id', 5)
+      table.string('website').notNullable()
+      table.string('comment_post').notNullable()
+      table.integer('user_id').unsigned()
+      table
+          .foreign('user_id')
+          .references('users.user_id')
       table.timestamps()
-      table
-      .foreign('user_id')
-      .references('users.user_id')
-      table
-      .foreign('community_id')
-      .references('communitys.community_id')
   })
 }
-  up() {
-      this.create('user_post_communities', (table) => {
-          table.increments('id', 5).notNullble()
-          table.integer('user_id').unsigned()
-          table.integer('communities_id').unsigned()
-          table
-              .foreign('user_id')
-              .references('users.user_id')
-          table
-              .foreign('community_id')
-              .references('communities.communities_id')
-      })
-  }
 
   down() {
     this.drop('user_post_communities')
   }
 }
 
-module.exports = CreateUserpostcommunitySchema
+module.exports = CreateCommunitySchema
