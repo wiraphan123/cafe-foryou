a6d307d0bcb28b51606b5832ad4772204ed509c0
pondatikan001 <pondatikan01@gmail.com>
2020-09-15 01:03:46 +0700

    edit util/cafe

--- database/util/CafeUtil.js
+++ database/util/CafeUtil.js
@@ -38,6 +38,36 @@ class CafeUtil {
             .then(response => response.first())
     }
 
+    async deletById(cafe){
+        const { id } = cafe.params
+        const cafe = await this._Cafe.find(id)
+
+        if(!cafe){
+            return {status : 500 ,error : `Not Found ${id}` , data : undefined};
+        }
+        cafe.delete()
+        await cafe.save();
+
+        return {status : 200 ,error : undefined , data : 'complete'};
+    }
+
+    async updateById(cafe,references){
+        const { id } = cafe.params
+        let cafe = await this._Cafe.find(id)
+
+        if(!cafe){
+            return {status : 500 ,error : `Not Found ${id}` , data : undefined};
+        }
+
+        cafe.merge(cafe.body)
+        await cafe.save();
+    
+        cafe = this._Cafe.query().where({cafe_id : id})
+        
+        return this._withReferences(community,references).fetch().then(response => response.first())
+    }
+
+
     _withReference(instance, references) {
         if (references) {
             const extractedReferences = references.split(",")
