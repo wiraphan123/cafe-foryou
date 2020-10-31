da415189ee774cfbb8136b93c624bc0432455b53
pondatikan001 <pondatikan01@gmail.com>
2020-10-31 23:56:10 +0700

    edit UserValidator.js

--- service/UserValidator.js
+++ service/UserValidator.js
@@ -1,27 +1,42 @@
-const Validator = use('Validator')
-module.exports = async function adminValidator(data) {
-    if (typeof data !== 'object') throw new Error()
-    const { first_name, last_name, age, gender, user_name, password } = data
-    const rules = {
-        first_name: 'required',
-        last_name: 'required',
-        age: 'required',
-        gender: 'required',
-        user_name: 'required',
-        password: 'required|min:8',
-        
-    }
+const Validator = use("Validator");
+module.exports = async function UserValidator(data) {
+  if (typeof data !== "object") throw new Error();
+  const {
+    first_name,
+    last_name,
+    age,
+    gender,
+    day_month_year,
+    username,
+    password,
+    status,
+  } = data;
+  const rules = {
+    first_name: "required",
+    last_name: "required",
+    age: "required",
+    gender: "required",
+    day_month_year: "required",
+    username: "required",
+    password: "required|min:8",
+    status: "required",
+  };
 
-    const validation = await Validator.validateAll({
-        first_name,
-        last_name,
-        age,
-        gender,
-        user_name,
-        password,
-    }, rules)
+  const validation = await Validator.validateAll(
+    {
+      first_name,
+      last_name,
+      age,
+      gender,
+      day_month_year,
+      username,
+      password,
+      status,
+    },
+    rules
+  );
 
-    return {
-        error: validation.messages()
-    }
-} 
+  return {
+    error: validation.messages(),
+  };
+};
